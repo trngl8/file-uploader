7579464cbc0e226b437b2ff46485f5d1240e9bbd
Red Rubber Duck <122934358+roman-duck@users.noreply.github.com>
2023-12-28 22:10:49 +0200

    refactoring

--- upload.php
+++ upload.php
@@ -3,29 +3,36 @@
 $uploadDir = 'uploads/';
 $fileName = 'uploaded_file';
 
-if (!empty($_FILES['file']) && isset($_POST['totalChunks']) && isset($_POST['currentChunk'])) {
-    $totalChunks = $_POST['totalChunks'];
-    $currentChunk = $_POST['currentChunk'];
-    $tempFileName = $uploadDir . $fileName . '_' . $currentChunk;
-
-    move_uploaded_file($_FILES['file']['tmp_name'], $tempFileName);
-
-    if ($currentChunk == $totalChunks - 1) {
-        $finalFileName = $uploadDir . $fileName;
-        $finalFile = fopen($finalFileName, 'wb');
-
-        for ($i = 0; $i < $totalChunks; $i++) {
-            $tempFileName = $uploadDir . $fileName . '_' . $i;
-            $chunk = file_get_contents($tempFileName);
-            fwrite($finalFile, $chunk);
-            unlink($tempFileName);
-        }
-
-        fclose($finalFile);
-        echo json_encode(['status' => 'success', 'message' => 'File uploaded successfully']);
-    } else {
-        echo json_encode(['status' => 'success', 'message' => 'Chunk uploaded successfully']);
-    }
-} else {
-    echo json_encode(['status' => 'error', 'message' => 'Invalid request']);
+if (empty($_FILES['file'])) {
+    echo json_encode(['status' => 'error', 'message' => 'No file uploaded']);
+    exit;
 }
+
+if(!isset($_POST['totalChunks']) || !isset($_POST['currentChunk'])) {
+    echo json_encode(['status' => 'error', 'message' => 'Invalid request']);
+    exit;
+}
+
+$totalChunks = $_POST['totalChunks'];
+$currentChunk = $_POST['currentChunk'];
+$tempFileName = $uploadDir . $fileName . '_' . $currentChunk;
+
+move_uploaded_file($_FILES['file']['tmp_name'], $tempFileName);
+
+if ($currentChunk == $totalChunks - 1) {
+    $finalFileName = $uploadDir . $fileName;
+    $finalFile = fopen($finalFileName, 'wb');
+
+    for ($i = 0; $i < $totalChunks; $i++) {
+        $tempFileName = $uploadDir . $fileName . '_' . $i;
+        $chunk = file_get_contents($tempFileName);
+        fwrite($finalFile, $chunk);
+        unlink($tempFileName);
+    }
+
+    fclose($finalFile);
+    echo json_encode(['status' => 'success', 'message' => 'File uploaded successfully']);
+}
+
+echo json_encode(['status' => 'success', 'message' => 'Chunk uploaded successfully']);
+
